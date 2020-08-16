2cab4513ed71a6197fdb3128a840c959a2242c8f
slurio <samantha.lurio@gmail.com>
2020-08-16 10:02:52 -0400

    update

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,22 +4,29 @@ class Teacher < ActiveRecord::Base
     has_many :apples
 
     def tenure
-        self.years_of_experience > 5 #need in string?
+        self.years_of_experience > 5 
     end
 
     def apple_count
         self.apples.count
     end
 
+    def how_many_ripe?
+        apples.where(status: "ripe").count
+    end
+
     def how_many_apples?
         puts "#{self.last_name} has #{self.apple_count} apples!"
+        sleep (1)
+        puts "#{self.how_many_ripe?} of the apples are ripe and ready to eat!"
     end
 
     def apples_ready_to_eat
         #apples.select do |apple|
          #   apple.status == "ripe"
         #end
-        Apple.all.where(status: "ripe")
+       # Apple.all.where(status: "ripe")
+        self.apples.where(status: "ripe")
     end
 
     def eat_apple(apple)
